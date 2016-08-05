13159726252477647c3c62499c570b9decc590d1
Sam Vermeulen <sam.verm@yahoo.com>
2016-08-05 19:40:40 +0200

    first commit

---
+++ app/src/main/java/com/chiro/sam/chirodrank/model/UserContent.java
@@ -0,0 +1,10 @@
+package com.chiro.sam.chirodrank.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserContent {
+
+    private static final List<User> ITEMS = new ArrayList<>();
+
+}
